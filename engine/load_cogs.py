844958b325708badb10000d028091a9d8e4b328b
arda <arda1033@gmail.com>
2023-09-22 01:16:32 +0300

    removed core cogs, all cogs are now modular

--- engine/load_cogs.py
+++ engine/load_cogs.py
@@ -1,9 +1,8 @@
-import os
 from nextcord.ext.commands import Bot
 from config import VERSION, NAME, cogs_to_load
 
 
-def tree_print(category:str = None, cog:str = None, last_category:bool = False, last_cog:bool = False):
+def tree_print(category: str = None, cog: str = None, last_category: bool = False, last_cog: bool = False):
     """
     Usage:
 
@@ -17,7 +16,7 @@ def tree_print(category:str = None, cog:str = None, last_category:bool = False, 
 
     if category != None:
         # print category
-    
+
         category_indent = last_branch if last_category else branch
 
         print(f"{category_indent}{category}")
@@ -30,62 +29,32 @@ def tree_print(category:str = None, cog:str = None, last_category:bool = False, 
 
         print(f"{category_indent}{cog_indent}{cog}")
 
-def load_cogs(bot):
 
-    load_core_cogs(bot)
-    load_other_cogs(bot)
-
-
-def load_core_cogs(bot: Bot):
-    cog_names = []
+def load_cogs(bot: Bot):
 
     print(f"Loading {NAME} v{VERSION}...\n")
-
     print(F"{NAME} v{VERSION}")
 
-    tree_print(category="core")
-
-    for file in os.listdir("core/"):
-        if file.endswith(".py"):
-            cog_names.append(file[:-3])
-    
-    cogs_imported = 0
-    for cog in cog_names:
-        bot.load_extension(f"core.{cog}")
-        cogs_imported += 1
-        tree_print(cog=cog, last_cog=cogs_imported == len(cog_names))
-
-
-def load_other_cogs(bot:Bot):
-    
     categories = []
     category_imported = 0
     for category in cogs_to_load:
         categories.append(category)
 
-
     for category in cogs_to_load:
         category_cogs = 0
         category_imported += 1
 
-        tree_print(category=category, last_category=category_imported == len(categories))
+        tree_print(category=category,
+                   last_category=category_imported == len(categories))
 
         for _ in cogs_to_load[category]:
             category_cogs += 1
 
-
         for cog_name, opts in cogs_to_load[category]:
             bot.load_extension(f"cogs.{category}.{cog_name}", extras=opts)
             category_cogs -= 1
-            tree_print(cog=cog_name, last_cog=category_cogs == 0, last_category=category_imported == len(categories))
-
-            
-
-                
-
-    
-
-    
+            tree_print(cog=cog_name, last_cog=category_cogs == 0,
+                       last_category=category_imported == len(categories))
 
 
 "├──"

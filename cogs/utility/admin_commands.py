8f99e55d7ac7d9d22f899b4afa373811124e80d9
arda <arda1033@gmail.com>
2023-09-22 02:05:18 +0300

    added admin commands

---
+++ cogs/utility/admin_commands.py
@@ -0,0 +1,33 @@
+from nextcord.ext import commands
+from nextcord.ext.commands import Bot, Context
+from engine.bot import bot
+
+
+class AdminCmds(commands.Cog):
+    def __init__(self, bot: Bot):
+        self.bot = bot
+
+    @commands.is_owner()
+    @commands.command()
+    async def servers(self, ctx: Context):
+        msg = ""
+        index = 0
+        for guild in bot.guilds:
+            msg += f"{index}. {guild.name} - {guild.owner}\n"
+            index += 1
+        await ctx.send(msg)
+
+    @commands.is_owner()
+    @commands.command()
+    async def reload_cogs(self, ctx: Context, *, cog_name: str = None):
+        if cog_name not in ["", None]:
+            try:
+                self.bot.reload_extension(cog_name)
+                await ctx.send(f"Reloaded `{cog_name}` successfully.")
+            except Exception as e:
+                await ctx.send(f"Error reloading {cog_name}:\n```{e}```")
+        else:
+            await ctx.send("Please specify a cog to reload")        
+
+def setup(bot: Bot):
+    bot.add_cog(AdminCmds(bot))

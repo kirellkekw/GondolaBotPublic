8e35bdbe24d0df2b5594ded0d507bbd44dc41597
arda <arda1033@gmail.com>
2023-09-22 11:41:59 +0300

    updated track messages

--- cogs/events/on_message/track_messages.py
+++ cogs/events/on_message/track_messages.py
@@ -1,26 +1,46 @@
-from nextcord import Message
+from nextcord import Message, Embed
 from nextcord.ext import commands
-from nextcord.ext.commands import Bot
+from nextcord.ext.commands import Bot, Context
 
-
-track_messages = False  # change value if you want messages to be tracked
-track_all_messages = False  # change value if you want to track only user messages
+track_message_count = True
+print_messages = False  # change value if you want messages to be tracked
+print_all_messages = False  # change value if you want to track only user messages
 
 
 class TrackMessages(commands.Cog):
-    def __init__(self, bot: Bot):        
+    all_messages = 0
+    user_messages = 0
+
+    def __init__(self, bot: Bot):
         self.bot = bot
-        
+
     @commands.Cog.listener()
     async def on_message(self, msg: Message):
-        if track_messages == True:
-            if track_all_messages:
+
+        if print_messages:
+            if print_all_messages:
                 print(
                     f"{msg.guild.name} /// {msg.channel.name} /// {msg.author} /// {msg.content}")
             else:
                 if not msg.author.bot:
                     print(
                         f"{msg.guild.name} /// {msg.channel.name} /// {msg.author} /// {msg.content}")
+
+        if track_message_count:
+            if not msg.author.bot:
+                self.user_messages += 1
+            self.all_messages += 1
+
+    @commands.is_owner()
+    @commands.command(hidden=True)
+    async def messages(self, ctx: Context):
+        em = Embed(title="Messages processed since last bootup",
+                   color=ctx.author.color)
+        em.add_field(name="All", value=self.all_messages)
+        em.add_field(name="User", value=self.user_messages)
+        em.add_field(name="Bot", value=self.all_messages - self.user_messages)
+        await ctx.send(embed=em)
+
 
 def setup(bot: Bot):
     bot.add_cog(TrackMessages(bot))

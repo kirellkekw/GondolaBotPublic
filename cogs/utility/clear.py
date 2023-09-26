1982262b99902b70a057998b3c4755dd8ee05560
arda <arda1033@gmail.com>
2023-09-26 20:03:43 +0300

    fixed clear command error

--- cogs/utility/clear.py
+++ cogs/utility/clear.py
@@ -6,12 +6,13 @@ from nextcord.ext.commands import Bot, Context, MissingPermissions
 @commands.has_permissions(manage_messages=True)
 async def clear(ctx: Context, amount: int = 1):
     try:
+        if not ctx.me.guild_permissions.manage_messages:
+            await ctx.send("I don't have permission to do that! Please give me the `Manage Messages` permission to use this command.")
+            return
         await ctx.channel.purge(limit=amount + 1)
         await ctx.send(f"Deleted {amount} messages by request of {ctx.author.mention}", delete_after=5)
     except MissingPermissions:
         await ctx.send("You don't have permission to do that!")
-    except not ctx.me.guild_permissions.manage_messages:
-        await ctx.send("I don't have permission to do that! Please give me the `Manage Messages` permission to use this command.")
 
 def setup(bot: Bot):
     bot.add_command(clear)

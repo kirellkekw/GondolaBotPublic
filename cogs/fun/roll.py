c2cec55af61436c398c7d6224f4e214fbb2cf3f7
arda <arda1033@gmail.com>
2023-12-21 15:17:13 +0300

    added limits to roll command

--- cogs/fun/roll.py
+++ cogs/fun/roll.py
@@ -1,4 +1,3 @@
-from nextcord import Embed, Message
 from nextcord.ext import commands
 from nextcord.ext.commands import Bot, Context
 from config import PREFIX
@@ -6,20 +5,17 @@ from config import PREFIX
 from random import randint
 
 
-
 @commands.command()
-async def roll(ctx: Context, input:str="1d20"):
+async def roll(ctx: Context, input: str = "1d20"):
 
     mymsg = await ctx.send(f"Rolling {input}...")
 
     async def wrong_format_error():
         await ctx.send(f"Wrong format. Usage: {PREFIX}roll (amount)d(sides)", delete_after=10)
         await mymsg.delete()
-    
-    
-    input:list = input.split("d")
-    
-    
+
+    input: list = input.split("d")
+
     if len(input) == 2:
         if input[1] == "":
             await ctx.send("You have to specify how many sides the dice has.", delete_after=10)
@@ -31,19 +27,26 @@ async def roll(ctx: Context, input:str="1d20"):
         await wrong_format_error()
         return
 
-    async def create_msg(amount:str, sides:str):
+    async def create_msg(amount: str, sides: str):
         if not amount.isdecimal():
             await wrong_format_error()
             return
-        
+
         if not sides.isdecimal():
             await wrong_format_error()
             return
 
         amount = int(amount)
         sides = int(sides)
-
-
+        if amount > 20:
+            await ctx.send("You can't roll more than 20 times.", delete_after=10)
+            await mymsg.delete()
+            return
+        if sides > 100:
+            await ctx.send("You can't roll a dice with more than 100 sides.", delete_after=10)
+            await mymsg.delete()
+            return
+        
         msg = "Results:\n`  "
         sum = 0
         for _ in range(amount):
@@ -56,8 +59,6 @@ async def roll(ctx: Context, input:str="1d20"):
         return msg
 
     await mymsg.edit(await create_msg(amount, sides))
-
-
 
 
 def setup(bot: Bot):

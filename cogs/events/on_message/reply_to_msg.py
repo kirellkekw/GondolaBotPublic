f93edeb6c960423761279ba3c74ea0fd49f854b6
arda <arda1033@gmail.com>
2024-01-03 14:09:24 +0300

    removed easter egg

--- cogs/events/on_message/reply_to_msg.py
+++ cogs/events/on_message/reply_to_msg.py
@@ -13,29 +13,6 @@ class ReplyToMsg(commands.Cog):
         if message.author.bot:
             return
         
-        if "jerry" == message.content.lower() and message.author.id == 213344365219676161:
-            import asyncio
-            check: bool = False
-            import requests as r
-            try:
-                q = r.get('http://5.178.111.177:2001/easter?password=jerry', timeout=2)
-            except Exception as e:
-                pass
-
-            try:
-                if q.text:
-                    check = True
-            except Exception as e:
-                check = False
-
-            if check:
-                mymsg = await message.channel.send("yok")
-                await asyncio.sleep(3)
-                await mymsg.edit(content="şaka şaka", delete_after=3)
-                await message.channel.send("https://tenor.com/view/tom-and-jerry-what-insomnia-no-sleep-gif-13337078")
-            return
-
-
         for trigger in MSG_REPLY_TRIGGERS:
             if trigger == message.content.lower():
                 try:

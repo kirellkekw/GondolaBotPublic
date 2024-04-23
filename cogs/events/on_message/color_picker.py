539e2c53b7249da32224f2f5fdbbd52bcb0b670e
arda <arda1033@gmail.com>
2024-04-24 02:43:19 +0300

    replaced request checking with regex

--- cogs/events/on_message/color_picker.py
+++ cogs/events/on_message/color_picker.py
@@ -1,8 +1,9 @@
 from nextcord import Message, Embed, Color
 from nextcord.ext import commands
 from nextcord.ext.commands import Bot
-import requests
+import re
 
+rgx = r"(?<=#){1}([a-fA-F0-9]{6})(?!.)"
 
 class ColorPicker(commands.Cog):
     def __init__(self, bot: Bot):
@@ -13,25 +14,26 @@ class ColorPicker(commands.Cog):
         if msg.author.bot:
             return
 
-        if msg.content.startswith("#") and len(msg.content) == 7:
-            res = requests.get(f"https://www.colorhexa.com/{msg.content[1::]}.png")
-            if res.status_code == 200:
-                try:
-                    em = Embed(
-                        title=f"#{msg.content[1::]}",
-                        color=Color.from_rgb(
-                            int(msg.content[1:3], 16),
-                            int(msg.content[3:5], 16),
-                            int(msg.content[5:7], 16),
-                        ),
-                        url=f"https://www.colorhexa.com/{msg.content[1::]}",
-                    )
-                    em.set_image(
-                        url=f"https://www.colorhexa.com/{msg.content[1::]}.png"
-                    )
-                    await msg.reply(embed=em)
-                except Exception as e:
-                    pass
+        match = re.findall(rgx, msg.content)
+
+        if not match:
+            return
+        try:
+            em = Embed(
+                title=f"#{match[0]}",
+                color=Color.from_rgb(
+                    int(match[0][:2], 16),
+                    int(match[0][2:4], 16),
+                    int(match[0][4:], 16),
+                ),
+                url=f"https://www.colorhexa.com/{match[0]}",
+            )
+            em.set_image(
+                url=f"https://www.colorhexa.com/{match[0]}.png"
+            )
+            await msg.reply(embed=em)
+        except Exception as e:
+            pass
 
 
 def setup(bot: Bot):
